773564b2f984f44330c1d4e2199a967c0c7ea37d
Yurii | monok8i 🦋 <didkivskiyyura725@gmail.com>
2025-06-09 14:45:36 +0000

    style: add docstrings to public modules and functions

--- __main__.py
+++ __main__.py
@@ -1,8 +1,17 @@
+"""Main entry point for the application."""
+
 from src.config.env import APP_STAGE, ENV_PATH
 from src.utils.logging import setup_logging
 
 
 def main() -> None:
+    """
+    Main entry point for the application.
+
+    Initializes logging, logs the application start,
+    current stage, and the path from which environment variables are loaded.
+    """
+
     logger = setup_logging()
     logger.info("Starting the application...")
     logger.info(f"App stage: {APP_STAGE}")
